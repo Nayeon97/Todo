62af9433f9384dd344b712a5a57cb26e210c57f3
Naayeon <naaay097@gmail.com>
2022-08-03 01:55:05 +0900

    ✨ Feat(custom): input style 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface InputProps {
   name: string;
@@ -8,7 +9,26 @@ interface InputProps {
 }
 
 const Input = ({ name, value, type, onChange }: InputProps) => {
-  return <input name={name} value={value} onChange={onChange} type={type} />;
+  return (
+    <InputWrapper name={name} value={value} onChange={onChange} type={type} />
+  );
 };
 
 export default Input;
+
+const InputWrapper = styled.input`
+  width: 300px;
+  height: 20px;
+  color: black;
+  background-color: #f1f3f5;
+  padding: 20px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+  font-size: 15px;
+  cursor: text;
+  &:focus::-webkit-input-placeholder {
+    color: #748ffc;
+  }
+`;
