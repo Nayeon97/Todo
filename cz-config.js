5c9403125dc50aa465979dad5e1d800c9bece574
Naayeon <naaay097@gmail.com>
2022-08-02 22:50:06 +0900

    🔧 Chore(custom): custom 커밋 삭제 여부 확인

--- cz-config.js
+++ cz-config.js
@@ -1,23 +1,23 @@
 module.exports = {
   types: [
-    { value: '✨ Feat', name: '✨ Feat:\t새로운 기능 추가' },
+    { value: '✨ Feat', name: '✨ Feat: 새로운 기능 추가' },
     {
       value: '🔥 Remove',
-      name: '🔥 Remove:\t코드, 파일 삭제',
+      name: '🔥 Remove: 코드, 파일 삭제',
     },
-    { value: '🐛 Modify', name: '🐛 Fix:\t코드 수정' },
-    { value: '📝 Docs', name: '📝 Docs:\t문서, 주석 수정' },
+    { value: '🐛 Modify', name: '🐛 Fix: 코드 수정' },
+    { value: '📝 Docs', name: '📝 Docs: 문서, 주석 수정' },
     {
       value: '💄 Style',
-      name: '💄 Style:\t코드 스타일, 포맷팅 정정',
+      name: '💄 Style: 코드 스타일, 포맷팅 정정',
     },
     {
       value: '🤖 Refactor',
-      name: '🤖 Refactor:\t코드 리팩터링',
+      name: '🤖 Refactor: 코드 리팩터링',
     },
     {
       value: '🔧 Chore',
-      name: '🔧 Chore:\t기타 변경사항(라이브러리 추가/수정..)',
+      name: '🔧 Chore: 기타 변경사항(라이브러리 추가/수정)',
     },
   ],
   allowCustomScopes: false,
